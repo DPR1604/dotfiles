cbda2ab7a528ff0c3f0fda0d1120097446c3391a
Gareth 'Gaz' Jones <garethjones1604@gmail.com>
2025-09-05 11:36:11 +0100

    ADDED: undotree

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -6,6 +6,9 @@ return {
     'tpope/vim-fugitive',
   },
   {
+    'mbbill/undotree',
+  },
+  {
     'brenoprata10/nvim-highlight-colors',
     config = function()
       require('nvim-highlight-colors').setup({})
