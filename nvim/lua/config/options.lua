ae6b8772b120a2eab6572f4fb1387744f87c087e
Gareth 'Gaz' Jones <garethjones1604@gmail.com>
2025-12-04 12:13:38 +0000

    ADDED: some quality of life options

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,27 @@
+-- tabs & indentation
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.smarttab = true
+
+-- ui
 vim.opt.number = true
 vim.opt.cursorline = true
-vim.opt.shiftwidth = 2
+vim.opt.signcolumn = "yes:1"
+vim.opt.scrolloff = 8
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
+
+-- backups and clipboard
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.clipboard = "unnamed"
+
+-- search
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
