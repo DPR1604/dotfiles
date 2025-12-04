b51522ea19cff1162beb3e47fa98618532240a19
Gareth 'Gaz' Jones <garethjones1604@gmail.com>
2025-12-04 12:17:21 +0000

    UPDATED treesitter config to fix highlighting issue

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,24 +1,52 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPre", "BufNewFile" },
   build = ":TSUpdate",
+  dependencies = {
+    "windwp/nvim-ts-autotag",
+  },
   config = function()
-    local configs = require("nvim-treesitter.configs")
-    configs.setup({
-      highight = { enable = true },
-      indent = { enable = true },
-      autotage = { enable = true },
-      ensure_installed = {
-	"lua",
-	"yaml",
-	"python",
-	"bash",
-	"terraform",
-	"html",
-	"css",
-	"dockerfile",
-	"markdown",
+    -- import nvim-treesitter plugin
+    local treesitter = require("nvim-treesitter.configs")
+
+    -- configure treesitter
+    treesitter.setup({ -- enable syntax highlighting
+      highlight = {
+        enable = true,
       },
-      auto_install = true,
+      -- enable indentation
+      indent = { enable = true },
+      -- ensure these language parsers are installed
+      ensure_installed = {
+        "python",
+	      "lua",
+	      "yaml",
+	      "bash",
+	      "terraform",
+	      "html",
+	      "css",
+	      "dockerfile",
+	      "markdown",
+        "json",
+        "markdown",
+        "markdown_inline",
+        "bash",
+        "vim",
+        "gitignore",
+        "vimdoc",
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
     })
-  end
+
+    -- use bash parser for zsh files
+    vim.treesitter.language.register("bash", "zsh")
+  end,
 }
