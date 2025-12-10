ef74fe334b871ae101ca3ac96276e668db8f5a4d
Gareth 'Gaz' Jones <gareth@valhallaonline.info>
2025-12-10 14:08:44 +0000

    UPDATED: folder structure to simplfiy use of stow

---
+++ .config/nvim/lua/plugins/lsp/mason.lua
@@ -0,0 +1,15 @@
+return {
+  "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "lua_ls",
+      },
+    },
+  dependencies = {
+    {
+      "williamboman/mason.nvim",
+      opts = {}
+    },
+    "neovim/nvim-lspconfig"
+  },
+}
